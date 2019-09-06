b156c48688dfeee3b38ed8af1ffc670d7cdf67a8
yuhang <yuhang@Laptop2.lan>
2019-09-06 16:19:09 -0400

    Setup Eureka service, register stock-service and db-service.

--- db-service/src/main/java/com/yuhang/stock/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/yuhang/stock/dbservice/DbServiceApplication.java
@@ -2,10 +2,12 @@ package com.yuhang.stock.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.yuhang.stock.dbservice.repository")
 @SpringBootApplication
+@EnableEurekaClient
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
